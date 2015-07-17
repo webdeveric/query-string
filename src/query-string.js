300cf386b327cc8f262c365afab9ce1e4c68c157
Eric King <eric.king@lonelyplanet.com>
2015-07-17 11:00:21 -0500

    Added splitParameter function

--- src/query-string.js
+++ src/query-string.js
@@ -1,53 +1,93 @@
-const parameters = {},
-      keyLength = {};
+const parameters = {};
+
+function isArray( obj )
+{
+  return Array.isArray ? Array.isArray( obj ) : Object.prototype.toString.call( obj ) === '[object Array]';
+}
+
+function toArray( obj )
+{
+  const data = [];
+
+  for ( let key in obj ) {
+    if ( obj.hasOwnProperty( key ) ) {
+      data[ key ] = obj[ key ];
+    }
+  }
+
+  return data;
+}
+
+function splitParameter( param )
+{
+  const eq = param.indexOf('='),
+        data = {
+          key: decodeURIComponent( eq === -1 ? param : param.substring( 0, eq ) ),
+          value: decodeURIComponent( eq === -1 ? '' : param.substring( eq + 1 ) ),
+          index: null
+        },
+        parts = data.key.match(/([\w\d_-]+)(\[([\w\d\+\s_-]*)\])?/i);
+
+  if ( parts !== null ) {
+
+    if ( parts[1] !== void 0 ) {
+      data.key = parts[1];
+    }
+
+    if ( parts[3] !== void 0 ) {
+      data.index = parts[3];
+    }
+
+  }
+
+  return data;
+}
 
 function parseParameter( param )
 {
-  if ( param === '' ) {
+  if ( ! param ) {
     return;
   }
 
-  let arg  = param.split('='),
-      val  = decodeURIComponent( arg[ 1 ] || '' ),
-      part = decodeURIComponent( arg[ 0 ] ).match(/([\w_]+)(\[([\w\+\s_-]*)\])?/i);
+  const { key, value, index } = splitParameter( param );
 
-  if ( part === null ) {
-    return;
-  }
-
-  let key      = part[1],
-      keyIndex = part[3];
-
-  if ( keyIndex !== void 0 ) {
+  if ( index !== void 0 ) {
 
     if ( ! parameters[ key ] ) {
-      parameters[ key ] = {};
-      keyLength[ key ] = 0;
+      parameters[ key ] = index === '' || /\d+/.test( index ) ? [] : {};
     }
 
     switch ( true ) {
-      case keyIndex === '':
-        parameters[ key ][ keyLength[ key ]++ ] = val;
+      case index === '':
+
+        if ( ! isArray( parameters[ key ] ) ) {
+          parameters[ key ] = toArray( parameters[ key ] );
+        }
+
+        parameters[ key ].push( value );
+
         break;
-      case /\d+/.test( keyIndex ):
-        parameters[ key ][ parseInt( keyIndex, 10 ) ] = val;
+      case /\d+/.test( index ):
+
+        parameters[ key ][ parseInt( index, 10 ) ] = value;
+
         break;
-      case /[\w\d\s_-]+/.test( keyIndex ):
-        parameters[ key ][ keyIndex ] = val;
+      case /[\w\d\+\s_-]+/.test( index ):
+
+        parameters[ key ][ index ] = value;
+
         break;
     }
 
   } else {
 
-    parameters[ key ] = val;
+    parameters[ key ] = value;
 
   }
 }
 
 if ( document.location.search !== '' ) {
-
   document.location.search.substr(1).split('&').forEach( parseParameter );
-
 }
 
 const queryString = {
